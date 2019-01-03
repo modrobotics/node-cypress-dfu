cc00cf638642190e89b250f549a511851b7a7ec5
Eric Lundby <Eric.Lundby@gmail.com>
2019-01-03 14:36:36 -0700

    Remove java code.

--- payloadProcessor.js
+++ payloadProcessor.js
@@ -9,15 +9,7 @@ var PayloadProcessor = function(path){
   processor.checkSumType = null
   processor.header = null
   processor.flashDataLines = [];
-
-  // fs.readFile(processor.filePath, 'utf8', function(err, contents) {
-  //   if(err){
-  //     throw err
-  //     return
-  //   }
-  //   console.log(contents)
-  //   processor.file = contents
-  // });
+  
   processor.file = fs.readFileSync(processor.filePath, 'utf8')
   processor.file = processor.file.trim()//Remove surrounding whitespace
 
@@ -56,7 +48,7 @@ var PayloadProcessor = function(path){
   }
 
   processor.getTotalLines = function(){
-
+    return processor.file.split(/\r?\n/).length
   }
 
   processor._getTheHeaderString = function(){
@@ -85,141 +77,3 @@ var PayloadProcessor = function(path){
 }
 
 module.exports = PayloadProcessor
-
-//
-// class CustomFileReader {
-//     private String mSiliconID;
-//     private final String mHeader;
-//     private final File mFile;
-//     private int mReadingLine = 0;
-//
-//     //File read status updater
-//     private FileReadStatusUpdater mFileReadStatusUpdaterUpdater;
-//
-//     //Constructor
-//     public CustomFileReader(String filepath) {
-//         mFile = new File(filepath);
-//         mHeader = getTheHeaderString(mFile);
-//         Logger.e("PATH>>>"+filepath);
-//     }
-//
-//     public void setFileReadStatusUpdater(FileReadStatusUpdater fileReadStatusUpdater) {
-//         this.mFileReadStatusUpdaterUpdater = fileReadStatusUpdater;
-//     }
-//
-//     /**
-//      * Analysing the header file and extracting the silicon ID,Check Sum Type and Silicon rev
-//      */
-//     public String[] analyseFileHeader() {
-//         String[] headerData = new String[3];
-//         String MSBString = Utils.getMSB(mHeader);
-//         mSiliconID = getSiliconID(MSBString);
-//         String mSiliconRev = getSiliconRev(MSBString);
-//         String mCheckSumType = getCheckSumType(MSBString);
-//         headerData[0] = mSiliconID;
-//         headerData[1] = mSiliconRev;
-//         headerData[2] = mCheckSumType;
-//         return headerData;
-//     }
-//
-//
-//     /**
-//      * Method to parse the file a read each line and put the line to a data model
-//      *
-//      * @return
-//      */
-//     public ArrayList<OTAFlashRowModel> readDataLines() {
-//         ArrayList<OTAFlashRowModel> flashDataLines = new ArrayList<OTAFlashRowModel>();
-//         String dataLine = null;
-//         try {
-//             BufferedReader bufferedReader = new BufferedReader(new FileReader(mFile));
-//             while ((dataLine = bufferedReader.readLine()) != null) {
-//                 mReadingLine++;
-//                 mFileReadStatusUpdaterUpdater.onFileReadProgressUpdate(mReadingLine);
-//                 byte[] data;
-//
-//                 OTAFlashRowModel model = new OTAFlashRowModel();
-//                 if (mReadingLine != 1) {
-//                     StringBuilder dataBuilder = new StringBuilder(dataLine);
-//                     dataBuilder.deleteCharAt(0);
-//                     model.mArrayId = Integer.parseInt(dataBuilder.substring(0, 2), 16);
-//                     model.mRowNo = Utils.getMSB(dataBuilder.substring(2, 6));
-//                     model.mDataLength = Integer.parseInt(dataBuilder.
-//                             substring(6, 10), 16);
-//                     model.mRowCheckSum = Integer.parseInt(dataBuilder.
-//                             substring(dataLine.length() - 3, dataLine.length() - 1), 16);
-//                     String datacharacters = dataBuilder.
-//                             substring(10, dataLine.length() - 2);
-//                     data = new byte[model.mDataLength];
-//                     for (int i = 0, j = 0; i < model.mDataLength; i++, j += 2) {
-//                         data[i] = (byte) Integer.parseInt(datacharacters.substring(j, j + 2), 16);
-//                     }
-//                     model.mData = data;
-//                     flashDataLines.add(model);
-//                 }
-//             }
-//         } catch (FileNotFoundException e) {
-//             e.printStackTrace();
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//         }
-//         return flashDataLines;
-//     }
-//
-//     /**
-//      * Method to count the total lines in the selected file
-//      *
-//      * @return totalLines
-//      */
-//     public int getTotalLines() {
-//         int totalLines = 0;
-//         String dataLine = "";
-//         try {
-//             BufferedReader bufferedReader = new BufferedReader(new FileReader(mFile));
-//             while ((dataLine = bufferedReader.readLine()) != null) {
-//                 totalLines++;
-//             }
-//         } catch (FileNotFoundException e) {
-//             e.printStackTrace();
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//         }
-//         return totalLines;
-//     }
-//
-//     /**
-//      * Reading the first line from the file
-//      *
-//      * @param file
-//      * @return
-//      */
-//     private String getTheHeaderString(File file) {
-//         String header = "";
-//         try {
-//             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-//             header = bufferedReader.readLine();
-//             bufferedReader.close();
-//         } catch (FileNotFoundException e) {
-//             e.printStackTrace();
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//         }
-//         return header;
-//     }
-//
-//     private String getSiliconID(String header) {
-//         String siliconID = header.substring(4, 12);
-//         return siliconID;
-//     }
-//
-//     private String getSiliconRev(String header) {
-//         String siliconRev = header.substring(2, 4);
-//         return siliconRev;
-//     }
-//
-//     private String getCheckSumType(String header) {
-//         String checkSumType = header.substring(0, 2);
-//         return checkSumType;
-//     }
-//
-// }
