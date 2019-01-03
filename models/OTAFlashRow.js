77cbecd32e9687176562f9d8c4629a9b7b535802
Eric Lundby <Eric.Lundby@gmail.com>
2019-01-03 14:29:39 -0700

    Data model for row of flash

---
+++ models/OTAFlashRow.js
@@ -0,0 +1,9 @@
+var OTAFlashRow = function(arrayID, rowNumber, dataLength, data, checksum){
+  this.arrayID = arrayID
+  this.rowNumber = rowNumber
+  this.dataLength = dataLength
+  this.data = data
+  this.checksum = checksum
+}
+
+module.exports = OTAFlashRow;
