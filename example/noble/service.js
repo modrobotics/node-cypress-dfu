0ff6a1647a67db43a81b0d04e99c459c7ed15bb7
Eric Lundby <Eric.Lundby@gmail.com>
2019-01-10 13:31:29 -0700

    Remove Cubelets specific code.

--- example/noble/service.js
+++ example/noble/service.js
@@ -16,20 +16,12 @@ var OTA_SERVICE_UUID = '00060000-F8CE-11E4-ABF4-0002A5D5C51B'
 // Bootloader Characteristic
 var OTA_COMMAND_UUID = '00060001-F8CE-11E4-ABF4-0002A5D5C51B'// Notify/Write
 
-// Application Service
-var APPLICATION_SERVICE_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'
-var APPLICATION_COMMAND_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
-
-var APPLICATION_MODE = 0
-var BOOTLOADER_MODE = 1
-
 var OTAService = function () {
   events.EventEmitter.call(this)
   var service = this
   service.state = 'notReady'
   service._scanning = false
   service.devices = {}
-  service.mode = APPLICATION_MODE
   service.writeCharacteristic = null
 
   noble.on('stateChange', function (state) {
@@ -50,17 +42,16 @@ var OTAService = function () {
       if (!peripheral || !peripheral.advertisement || !peripheral.advertisement.localName) {
         return
       }
-      if (peripheral.advertisement.localName.toLowerCase().indexOf('cubelet') === 0) {
-        peripheral.btType = 'le'
-        peripheral.name = peripheral.advertisement.localName
 
-        if (peripheral.address in service.devices) {
-          service.devices[peripheral.address] = peripheral
-          deviceUpdated(peripheral)
-        } else {
-          service.devices[peripheral.address] = peripheral
-          deviceAdded(peripheral)
-        }
+      peripheral.btType = 'le'
+      peripheral.name = peripheral.advertisement.localName
+
+      if (peripheral.address in service.devices) {
+        service.devices[peripheral.address] = peripheral
+        deviceUpdated(peripheral)
+      } else {
+        service.devices[peripheral.address] = peripheral
+        deviceAdded(peripheral)
       }
     }
 
@@ -98,31 +89,9 @@ var OTAService = function () {
   this.connect = function (device, callback) {
     device.once('connect', function () {
       debug('Connected...Discovering Services/Characteristics')
-      device.discoverSomeServicesAndCharacteristics([OTA_SERVICE_UUID, APPLICATION_SERVICE_UUID], [OTA_COMMAND_UUID, APPLICATION_COMMAND_UUID], function (err, services, characteristics) {
+      device.discoverSomeServicesAndCharacteristics([OTA_SERVICE_UUID], [OTA_COMMAND_UUID], function (err, services, characteristics) {
         if (err) {
           console.error(err)
-        }
-        if (service.mode === APPLICATION_MODE) {
-          debug('device is in application mode.')
-          var applicationCharacteristic = characteristics.find(characteristic => characteristic.uuid === formatUUID(APPLICATION_COMMAND_UUID))
-
-          device.once('disconnect', function () {
-            setTimeout(function () {
-              service.connect(device, callback)
-            }, 1000)
-          })
-
-          // Set to bootloader mode, disconnect, and reconnect in a second
-          var ar = []
-          ar[0] = '<'.charCodeAt(0)
-          ar[1] = 0x03
-          ar[2] = 1
-          ar[3] = '>'.charCodeAt(0)
-          ar[4] = 0
-          applicationCharacteristic.write(toBuffer(ar), true, function () {
-            service.mode = BOOTLOADER_MODE
-          })
-          return
         }
         service.writeCharacteristic = characteristics.find(characteristic => characteristic.uuid === formatUUID(OTA_COMMAND_UUID))
         service.writeCharacteristic.subscribe(function (err) {
