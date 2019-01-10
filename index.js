2708b3a22490f220658da77d82c3fc30e80e3340
Eric Lundby <Eric.Lundby@gmail.com>
2019-01-10 10:06:52 -0700

    Turn into module.

--- index.js
+++ index.js
@@ -1,90 +1,40 @@
-var OTAService = require('./ota_service.js')
-var otaService = new OTAService()
-
-var OTAUpdater = require('./ota_updater.js')
-var otaUpdater = new OTAUpdater(otaService)
-
 var PayloadProcessor = require('./payloadProcessor.js')
-var payloadProcessor = new PayloadProcessor('./test/bootloadable.cyacd')
-payloadProcessor.analyzeHeader()
-payloadProcessor.readDataLines()
+var OTAUpdater = require('./ota_updater.js')
+var events = require('events')
+var util = require('util');
 
-var ProgressBar = require('cli-progress');
-var progressBar = new ProgressBar.Bar({}, ProgressBar.Presets.shades_classic);
+var CypressDFU = function(){
+  var cypressDFU = this
 
-var _ = require('underscore');
+  this.startUpdate = function(payload, writeMethod){
+    this.otaUpdater = new OTAUpdater(writeMethod)
+    this.onData = this.otaUpdater.onData
+    this.attachEventListeners()
 
-//For Allowing user to select a device to update
-var list = require('select-shell')(
-  /* possible configs */
-  {
-    pointer: ' ▸ ',
-    pointerColor: 'yellow',
-    checked: ' ◉  ',
-    unchecked:' ◎  ',
-    checkedColor: 'blue',
-    msgCancel: 'No selected options!',
-    msgCancelColor: 'orange',
-    multiSelect: false,
-    inverse: true,
-    prepend: true,
-    disableInput: true
+    var payloadProcessor = new PayloadProcessor(payload)
+    payloadProcessor.analyzeHeader()
+    payloadProcessor.readDataLines()
+
+    this.otaUpdater.start(payloadProcessor)
+
+    return this.otaUpdater;
   }
-);
 
-var discoverTimeout = null;
-
-function deviceAdded(device){
-  clearTimeout(discoverTimeout)
-  discoverTimeout = setTimeout(function(){
-    _.each(otaService.devices, function (device, address) {
-      list.option(device.name+" ("+device.address+")", device)
-    });
-    console.log("Select a Cubelet Hat to flash:")
-    list.list();
-  }, 5000)
-}
-function deviceUpdated(device){
-  //
-}
-
-otaService.startDeviceScan(deviceAdded, deviceUpdated, function(err){
-
-})
-
-list.on('select', function(options){
-  var device = options[0].value
-
-  otaService.stopDeviceScan(function(){
-    otaService.connect(device, function(err){
-      if(err){
-        console.log("Connecting Failed, please try again.")
-        process.exit(0)
-      }
-
-      otaUpdater.start(payloadProcessor)
+  this.attachEventListeners = function(){
+    this.otaUpdater.on('progress', function(percentage){
+      cypressDFU.emit('progress', percentage)
     })
-  })
-});
+    this.otaUpdater.on('flashStart', function(){
+      cypressDFU.emit('flashStart')
+    })
+    this.otaUpdater.on('flashFinished', function(){
+      cypressDFU.emit('flashFinished')
+    })
+    this.otaUpdater.on('error', function(err, code, message){
+      cypressDFU.emit('error', err, code, message)
+    })
+  }
+}
 
-list.on('cancel', function(options){
-  console.log('Cancel list, '+ options.length +' options selected');
-  process.exit(0);
-});
-
-otaUpdater.on('progress', function(percentage){
-  progressBar.update(percentage)
-})
-otaUpdater.on('flashStart', function(percentage){
-  console.log("Flashing...")
-  progressBar.start(100,0)
-})
-otaUpdater.on('flashFinished', function(){
-  progressBar.stop()
-  console.log("Flashing...Success")
-  process.exit();
-})
-otaUpdater.on('error', function(err, code, message){
-  console.log(err, code, message)
-  process.exit()
-})
+util.inherits(CypressDFU, events.EventEmitter);
+module.exports = new CypressDFU()
